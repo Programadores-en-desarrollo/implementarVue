a4b2b8739049437355fabb0419d673ac41da56e3
Armando Sánchez Trabajando <32597371+Programadores-en-desarrollo@users.noreply.github.com>
2023-08-27 10:53:45 -0600

    Update principal.js
    agregar el código

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        // titulo : 'hola mundo'
+        nombre: '',
+        promedio: '',
+        lista: [ ]
+    },
+    methods: {
+        agregarNota: function(){
+            if (this.nombre!="" && this.promedio!="") {
+                this.lista.push({nombre: this.nombre, promedio: this.promedio})
+                this.nombre= '';
+                this.promedio = '';
 
+            } else {
+                alert('Nombre y promedio son obligatorios');
+            }
+        }
+    }
+})
